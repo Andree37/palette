df28203f131a3736d6248fe254d40320f651032a
Andree37 <andresmribeiro@gmail.com>
2023-01-16 19:23:58 +0000

    updated colors

--- index.js
+++ index.js
@@ -64,6 +64,83 @@ const dark = {
         rgb: 'rgb(245, 224, 220)',
         hsl: 'hsl(10, 56%, 91%)',
     },
+    flamingo: {
+        hex: '#f2cdcd',
+        rgb: 'rgb(242, 205, 205)',
+        hsl: 'hsl(0, 59%, 88%)',
+    },
+
+    mauve: {
+        hex: '#cba6f7',
+        rgb: 'rgb(203, 166, 247)',
+        hsl: 'hsl(267, 84%, 81%)',
+    },
+
+    red: {
+        hex: '#f38ba8',
+        rgb: 'rgb(243, 139, 168)',
+        hsl: 'hsl(343, 81%, 75%)',
+    },
+
+    teal: {
+        hex: '#94e2d5',
+        rgb: 'rgb(148, 226, 213)',
+        hsl: 'hsl(170, 57%, 73%)',
+    },
+
+    sky: {
+        hex: '#89dceb',
+        rgb: 'rgb(137, 220, 235)',
+        hsl: 'hsl(189, 71%, 73%)',
+    },
+
+    sapphire: {
+        hex: '#74c7ec',
+        rgb: 'rgb(116, 199, 236)',
+        hsl: 'hsl(199, 76%, 69%)',
+    },
+
+    subtext1: {
+        hex: '#bac2de',
+        rgb: 'rgb(186, 194, 222)',
+        hsl: 'hsl(227, 35%, 80%)',
+    },
+
+    subtext0: {
+        hex: '#a6adc8',
+        rgb: 'rgb(166, 173, 200)',
+        hsl: 'hsl(228, 24%, 72%)',
+    },
+
+    overlay2: {
+        hex: '#9399b2',
+        rgb: 'rgb(147, 153, 178)',
+        hsl: 'hsl(228, 17%, 64%)',
+    },
+
+    overlay1: {
+        hex: '#7f849c',
+        rgb: 'rgb(127, 132, 156)',
+        hsl: 'hsl(230, 13%, 55%)',
+    },
+
+    surface2: {
+        hex: '#585b70',
+        rgb: 'rgb(88, 91, 112)',
+        hsl: 'hsl(233, 12%, 39%)',
+    },
+
+    surface0: {
+        hex: '#313244',
+        rgb: 'rgb(49, 50, 68)',
+        hsl: 'hsl(237, 16%, 23%)',
+    },
+
+    mantle: {
+        hex: '#181825',
+        rgb: 'rgb(24, 24, 37)',
+        hsl: 'hsl(240, 21%, 12%)',
+    }
 }
 
 const palettes = {
@@ -122,6 +199,57 @@ const palettes = {
         max_yellow_red: {
             dark: dark.max_yellow_red,
         },
+        flamingo: {
+            dark: dark.flamingo,
+        },
+
+        mauve: {
+            dark: dark.mauve
+        },
+
+        red: {
+            dark: dark.red
+        },
+
+        teal: {
+            dark: dark.teal
+        },
+
+        sky: {
+            dark: dark.sky
+        },
+
+        sapphire: {
+            dark: dark.sapphire
+        },
+
+        subtext1: {
+            dark: dark.subtext1
+        },
+
+        subtext0: {
+            dark: dark.subtext0
+        },
+
+        overlay2: {
+            dark: dark.overlay2
+        },
+
+        overlay1: {
+            dark: dark.overlay1
+        },
+
+        surface2: {
+            dark: dark.surface2
+        },
+
+        surface0: {
+            dark: dark.surface0
+        },
+
+        mantle: {
+            dark: dark.mantle
+        }
     },
 }
 
